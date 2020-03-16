a6f49e6f4499f1990bd1cec9dbe7c97de12a6ef1
LinX9581 <32073475+LinX9581@users.noreply.github.com>
2020-03-16 13:55:22 +0800

    Update Fire.js

--- Fire.js
+++ Fire.js
@@ -9,12 +9,12 @@ class Fire {
     init = () => {
         if (!firebase.apps.length) {
             firebase.initializeApp({
-                apiKey: "AIzaSyAN29JToq7jGGiol4xRHWPk0lAB_U_kGPY",
-                authDomain: "artificial-intelligence-8da03.firebaseapp.com",
-                databaseURL: "https://artificial-intelligence-8da03.firebaseio.com",
-                projectId: "artificial-intelligence-8da03",
-                storageBucket: "artificial-intelligence-8da03.appspot.com",
-                messagingSenderId: "1095359509219"
+                apiKey: " ",
+                authDomain: " ",
+                databaseURL: " ",
+                projectId: " ",
+                storageBucket: " ",
+                messagingSenderId: " "
             });
         }
     };
